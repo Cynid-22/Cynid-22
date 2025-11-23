daa7d27ca615b90e34d6606ded287a9aad448b74
Cynid-22 <vunguyenkhang@pm.me>
2025-11-23 14:21:13 -0500

    feat: Automate README updates with a Python script to dynamically generate featured projects.

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -132,7 +132,6 @@ def update_readme():
 
   <p align="left">
 {chr(10).join(repo_cards)}
-      
   </p>
 
   <a href="https://github.com/{username}?tab=repositories&sort=stargazers"><img alt="All Repositories" title="All Repositories" src="https://custom-icon-badges.demolab.com/badge/-Click%20Here%20For%20All%20My%20Repos-1F222E?style=for-the-badge&logoColor=white&logo=repo"/></a>
